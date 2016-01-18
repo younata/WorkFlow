3bc86117413846895d66450c8065087bd66601cb
Rachel Brindle <you@subluminal.net>
2016-01-17 17:04:19 -0800

    Initial Commit

---
+++ WorkFlow/WorkFlow.h
@@ -0,0 +1,19 @@
+//
+//  WorkFlow.h
+//  WorkFlow
+//
+//  Created by Rachel Brindle on 1/17/16.
+//  Copyright © 2016 Rachel Brindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WorkFlow.
+FOUNDATION_EXPORT double WorkFlowVersionNumber;
+
+//! Project version string for WorkFlow.
+FOUNDATION_EXPORT const unsigned char WorkFlowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorkFlow/PublicHeader.h>
+
+
