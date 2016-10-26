f505854ac581f090a3131e525cfa36ca979400d4
Rachel Brindle <you@subluminal.net>
2016-10-26 10:53:23 -0700

    Add MacOS version of workflow

---
+++ WorkFlow-macOS/WorkFlow-macOS.h
@@ -0,0 +1,19 @@
+//
+//  WorkFlow-macOS.h
+//  WorkFlow-macOS
+//
+//  Created by Rachel Brindle on 10/26/16.
+//  Copyright © 2016 Rachel Brindle. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WorkFlow-macOS.
+FOUNDATION_EXPORT double WorkFlow_macOSVersionNumber;
+
+//! Project version string for WorkFlow-macOS.
+FOUNDATION_EXPORT const unsigned char WorkFlow_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorkFlow_macOS/PublicHeader.h>
+
+
